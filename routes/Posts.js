f28f88b65aee7085d86b58aa4edc8ea763b6728c
Ahsaan <ahsaan1345@outlook.com>
2025-11-09 18:04:22 +0530

    final changes

--- routes/Posts.js
+++ routes/Posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/Posts');
+const Post = require('../models/Post');
 
 // Create new post
 router.post('/', async (req, res) => {
