1e5a474cb112a17940eb99b8714d4a330bfa1796
Ahsaan <ahsaan1345@outlook.com>
2025-11-09 12:53:18 +0530

    updated the changes

--- learnato-forum-frontend/src/App.js
+++ learnato-forum-frontend/src/App.js
@@ -1,38 +1,42 @@
-  import React, { useEffect, useState } from 'react';
-  import PostList from './components/PostList';
-  import PostDetail from './components/PostDetail';
-  import socket from './socket';
+import React, { useEffect, useState } from 'react';
+import PostList from './components/PostList';
+import PostDetail from './components/PostDetail';
+import CreatePost from './components/CreatePost';
+import socket from './socket';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-  function App() {
-    const [selectedPostId, setSelectedPostId] = useState(null);
-    const [refreshKey, setRefreshKey] = useState(0); // to trigger PostList refresh
+function App() {
+  const [selectedPostId, setSelectedPostId] = useState(null);
+  const [refreshKey, setRefreshKey] = useState(0);
 
-    useEffect(() => {
-      socket.on('newPost', () => {
-        setRefreshKey(oldKey => oldKey + 1);
-      });
-      socket.on('newReply', () => {
-        setRefreshKey(oldKey => oldKey + 1);
-      });
-      socket.on('postUpvoted', () => {
-        setRefreshKey(oldKey => oldKey + 1);
-      });
-      return () => {
-        socket.off('newPost');
-        socket.off('newReply');
-        socket.off('postUpvoted');
-      };
-    }, []);
+  useEffect(() => {
+    socket.on('newPost', () => setRefreshKey(k => k + 1));
+    socket.on('newReply', () => setRefreshKey(k => k + 1));
+    socket.on('postUpvoted', () => setRefreshKey(k => k + 1));
+    return () => {
+      socket.off('newPost');
+      socket.off('newReply');
+      socket.off('postUpvoted');
+    };
+  }, []);
 
-    return (
-      <div className="max-w-4xl mx-auto p-4">
-        {!selectedPostId ? (
-          <PostList key={refreshKey} onSelectPost={setSelectedPostId} />
-        ) : (
-          <PostDetail postId={selectedPostId} onBack={() => setSelectedPostId(null)} />
-        )}
-      </div>
-    );
+  function handlePostCreated() {
+    setRefreshKey(k => k + 1);
   }
 
-  export default App;
+  return (
+    <div className="max-w-4xl mx-auto p-4">
+      {!selectedPostId && <CreatePost onPostCreated={handlePostCreated} />}
+      {!selectedPostId ? (
+        <PostList key={refreshKey} onSelectPost={setSelectedPostId} />
+      ) : (
+        <PostDetail postId={selectedPostId} onBack={() => setSelectedPostId(null)} />
+      )}
+
+      <ToastContainer position="top-right" autoClose={3000} />
+    </div>
+  );
+}
+
+export default App;
