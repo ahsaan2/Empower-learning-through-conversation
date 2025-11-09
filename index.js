4c67b0cb0cc20eaa94bf7d4156f1b655b4956564
Ahsaan <ahsaan1345@outlook.com>
2025-11-09 18:23:42 +0530

    final changes

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const http = require('http');
-const postsRoutes = require('../routes/Posts');
-
+const postsRoutes = require('./routes/posts');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
